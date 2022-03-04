06631689e53126fffb4099f53990f67943bfca78
gianmarios-ch <gseregni@companieshouse.gov.uk>
2022-03-04 16:41:24 +0000

    dsnd-493 receiveMainMessages method log improvement

--- src/main/java/uk/gov/companieshouse/charges/delta/consumer/ChargesDeltaConsumer.java
+++ src/main/java/uk/gov/companieshouse/charges/delta/consumer/ChargesDeltaConsumer.java
@@ -1,25 +1,25 @@
 package uk.gov.companieshouse.charges.delta.consumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.charges.delta.processor.ChargesDeltaProcessor;
 import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.logging.Logger;
 
 
 @Component
 public class ChargesDeltaConsumer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ChargesDeltaConsumer.class);
-
     private final ChargesDeltaProcessor deltaProcessor;
+    private final Logger logger;
 
     @Autowired
-    public ChargesDeltaConsumer(ChargesDeltaProcessor deltaProcessor) {
+    public ChargesDeltaConsumer(ChargesDeltaProcessor deltaProcessor, Logger logger) {
         this.deltaProcessor = deltaProcessor;
+        this.logger = logger;
     }
 
     /**
@@ -30,9 +30,8 @@ public class ChargesDeltaConsumer {
             groupId = "${charges.delta.group-id}",
             containerFactory = "listenerContainerFactory")
     public void receiveMainMessages(Message<ChsDelta> chsDeltaMessage) {
-        LOGGER.info("A new message read from MAIN topic with payload: "
-                + chsDeltaMessage.getPayload());
-        //deltaProcessor.processDelta(chsDeltaMessage);
+        logger.info(String.format("DSND-493: New message read from MAIN topic with payload: %s",
+                chsDeltaMessage.getPayload()));
     }
 
     /**
@@ -43,9 +42,9 @@ public class ChargesDeltaConsumer {
             groupId = "${charges.delta.group-id}",
             containerFactory = "listenerContainerFactory")
     public void receiveRetryMessages(Message<ChsDelta> message) {
-        LOGGER.info(String.format("A new message read from RETRY topic with payload:%s "
+        logger.info(String.format("A new message read from RETRY topic with payload:%s "
                 + "and headers:%s ", message.getPayload(), message.getHeaders()));
-        //deltaProcessor.processDelta(message);
+
     }
 
 }
