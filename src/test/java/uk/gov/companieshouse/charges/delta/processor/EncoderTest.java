bd6cf659b6e82aaf960e1730e212ebce4d3bf0e4
sampipparampil <99203120+sampipparampil@users.noreply.github.com>
2022-04-04 12:40:49 +0100

    Feature/dsnd 684 update mappings (#19)
    * dsnd-684: Updated mappings with notice types
    
    * dsnd-684: Updated mappings with notice types
    
    * dsnd-684: Updated mappings with notice types
    
    * dsnd-684: Updated mappings with notice types
    
    * dsnd-684: Updated mappings with notice types
    
    * dsnd-684: Added more tests with data variations
    
    * dsnd-684:Refactored values to constants
    
    * dsnd-684:Refactored MapperUtils class name to NoticeTypeMapperUtils
    
    * dsnd-684:resolved conflicts

--- src/test/java/uk/gov/companieshouse/charges/delta/processor/EncoderTest.java
+++ src/test/java/uk/gov/companieshouse/charges/delta/processor/EncoderTest.java
@@ -11,8 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @TestPropertySource(
         properties = {
-                "api.charge-id-salt=jkdhjdio",
-                "api.trans-id-salt=abcdefgh"
+                "api.charge-id-salt=sometest",
+                "api.trans-id-salt=sometest2"
         }
 )
 @ExtendWith(SpringExtension.class)
@@ -33,7 +33,7 @@ class EncoderTest {
 
     @Test
     void encode() {
-        String expectedValue = "CQ79y791IeRwkR3CNJMSOTNcWIA="
+        String expectedValue = "2e6X44GGkwsvYqCd5HF6a42mWZs="
                 .replace("+", "-")
                 .replace("/", "_");
         String encodeValue = encoder.encodeWithSha1("3000117455");
@@ -41,11 +41,19 @@ class EncoderTest {
     }
 
     @Test
-    void encode_ChargedIdThatGeneratesAValueWithSlash() {
-        String expectedValue = "0Vx0iTn_oJBRsITU1jbtTaEZElk="
-                .replace("+", "-")
-                .replace("/", "_");
-        String encodeValue = encoder.encodeWithSha1("3001283055");
+    void encodeWithoutSha1() {
+        String expectedValue = "MzAwMDExNzQ1NXNvbWV0ZXN0Mg==";
+        String encodeValue = encoder.encodeWithoutSha1("3000117455");
         assertThat(encodeValue).isEqualTo(expectedValue);
     }
+
+    @Test
+    void encode_charged_Id_which_generates_value_with_slash() {
+        String expectedValue = "613cXqSXAA1Ce_fpkFQ9ZP_L5ZQ=";
+        String encodeValue = encoder.encodeWithSha1("3101283055");
+        assertThat(encodeValue).isEqualTo(expectedValue);
+
+
+    }
+
 }
