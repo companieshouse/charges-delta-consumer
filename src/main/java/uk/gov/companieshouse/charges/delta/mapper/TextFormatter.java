76dd1c0af2fe3911ea91803b7cc783ef921ecbbf
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-06-08 10:02:30 +0100

    Code style fixes in TextFormatter utility class
    * Add Javadocs to TextFormatter.formatAsEntityName
    * Fix checkstyle violations e.g. import ordering, line length etc.

--- src/main/java/uk/gov/companieshouse/charges/delta/mapper/TextFormatter.java
+++ src/main/java/uk/gov/companieshouse/charges/delta/mapper/TextFormatter.java
@@ -1,9 +1,5 @@
 package uk.gov.companieshouse.charges.delta.mapper;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.text.StringTokenizer;
-import org.apache.commons.text.WordUtils;
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Locale;
@@ -11,19 +7,53 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringTokenizer;
+import org.apache.commons.text.WordUtils;
+
 public final class TextFormatter {
 
-    private static final Pattern STEM_PATTERN = Pattern.compile("(\\()(\\p{Alnum}+)");
-    private static final Pattern COLON_PATTERN = Pattern.compile("[;:]$");
-    private static final Pattern MIXED_ALNUM_PATTERN = Pattern.compile("\\p{L}+\\p{N}+|\\p{N}+\\p{L}+");
-    private static final Pattern ABBREVIATION_PATTERN = Pattern.compile("(\\p{L}[.])+");
+    private static final Pattern STEM_PATTERN =
+            Pattern.compile("(\\()(\\p{Alnum}+)");
+    private static final Pattern COLON_PATTERN =
+            Pattern.compile("[;:]$");
+    private static final Pattern MIXED_ALNUM_PATTERN =
+            Pattern.compile("\\p{L}+\\p{N}+|\\p{N}+\\p{L}+");
+    private static final Pattern ABBREVIATION_PATTERN =
+            Pattern.compile("(\\p{L}[.])+");
 
-    private static final Set<String> STOP_WORDS = new HashSet<>(Arrays.asList("A","AN","AT","AS","AND","ARE","BUT","BY","ERE","FOR","FROM","IN","INTO","IS","OF","ON","ONTO","OR","OVER","PER","THE","TO","THAT","THAN","UNTIL","UNTO","UPON","VIA","WITH","WHILE","WHILST","WITHIN","WITHOUT"));
-    private static final Set<String> ENTITIES = new HashSet<>(Arrays.asList("ARD","NI","SE","GB","SC","UK","LTD","L.T.D","PLC","P.L.C","UNLTD","CIC","C.I.C","LLP","L.P","LP","EEIG","OEIC","ICVC","AEIE","C.B.C","C.C.C","CBC","CBCN","CBP","CCC","CYF","EESV","EOFG","EOOS","GEIE","GELE","PAC","PCCLIMITED","PCCLTD","PROTECTEDCELLCOMPANY","CWMNICELLGWARCHODEDIG","CCGCYFYNGEDIG","CCGCYF"));
+    private static final Set<String> STOP_WORDS = new HashSet<>(Arrays.asList("A", "AN", "AT",
+            "AS", "AND", "ARE", "BUT", "BY", "ERE", "FOR", "FROM", "IN", "INTO", "IS", "OF", "ON",
+            "ONTO", "OR", "OVER", "PER", "THE", "TO", "THAT", "THAN", "UNTIL", "UNTO", "UPON",
+            "VIA", "WITH", "WHILE", "WHILST", "WITHIN", "WITHOUT"));
+    private static final Set<String> ENTITIES = new HashSet<>(Arrays.asList("ARD", "NI", "SE",
+            "GB", "SC", "UK", "LTD", "L.T.D", "PLC", "P.L.C", "UNLTD", "CIC", "C.I.C", "LLP",
+            "L.P", "LP", "EEIG", "OEIC", "ICVC", "AEIE", "C.B.C", "C.C.C", "CBC", "CBCN", "CBP",
+            "CCC", "CYF", "EESV", "EOFG", "EOOS", "GEIE", "GELE", "PAC", "PCCLIMITED", "PCCLTD",
+            "PROTECTEDCELLCOMPANY", "CWMNICELLGWARCHODEDIG", "CCGCYFYNGEDIG", "CCGCYF"));
 
-    private TextFormatter(){
+    private TextFormatter() {
     }
 
+    /**
+     * Format a given string as an entity name in accordance to the following rules
+     * ordered by precedence:
+     * <br>
+     * <ul>
+     * <li>format(a) == format(b) where a is case-insensitively equal to b</li>
+     * <li>If the provided string is null or empty, return the provided value as given.</li>
+     * <li>Any words within parentheses must use highlight casing.</li>
+     * <li>Any word proceeding a colon must use highlight casing.</li>
+     * <li>First and last words must use highlight casing.</li>
+     * <li>Any stop words in Appendix B must be lowercase.</li>
+     * <li>Any words containing both letters and numbers must be uppercase.</li>
+     * <li>Any entity names in Appendix C must be uppercase.</li>
+     * <li>Any country codes in Appendix D must be uppercase.</li>
+     * <li>All other words must use highlight casing.</li>
+     * </ul>
+     * @param text The text that will be recased.
+     * @return Text recased in accordance to the above rules.
+     */
     public static String formatAsEntityName(String text) {
         if (StringUtils.isEmpty(text)) {
             return text;
@@ -33,21 +63,21 @@ public final class TextFormatter {
         StringBuilder builder = new StringBuilder();
         int index = 0;
         FormatterStateMachine stateMachine = new FormatterStateMachine();
-        while(tokenizer.hasNext()) {
+        while (tokenizer.hasNext()) {
             String token = tokenizer.next();
             stateMachine.setToken(token);
             if (isEntity(token)) {
                 stateMachine.entityName();
-            } else if (isOpeningParentheses(token)) {
-                stateMachine.parentheses();
+            } else if (isOpeningParenthesis(token)) {
+                stateMachine.parenthesis();
             } else if (isStopWord(token, index, tokenizer.hasNext())) {
                 stateMachine.stopWord();
             } else {
-                stateMachine.hasText();
+                stateMachine.regularText();
             }
-            builder.append(stateMachine.getToken()).append(" ");
+            builder.append(stateMachine.getMappedToken()).append(" ");
             if (endsWithColon(token)) {
-                stateMachine.hasColon();
+                stateMachine.colon();
             }
             index++;
         }
@@ -57,10 +87,12 @@ public final class TextFormatter {
     private static boolean isEntity(String token) {
         Matcher mixedAlnumMatcher = MIXED_ALNUM_PATTERN.matcher(token);
         Matcher abbreviationMatcher = ABBREVIATION_PATTERN.matcher(token);
-        return ENTITIES.contains(token) || mixedAlnumMatcher.find() || abbreviationMatcher.matches();
+        return ENTITIES.contains(token)
+                || mixedAlnumMatcher.find()
+                || abbreviationMatcher.matches();
     }
 
-    private static boolean isOpeningParentheses(String token) {
+    private static boolean isOpeningParenthesis(String token) {
         Matcher tokenMatcher = STEM_PATTERN.matcher(token);
         return tokenMatcher.find();
     }
@@ -75,40 +107,38 @@ public final class TextFormatter {
     }
 
     private static class FormatterStateMachine {
-        private final NoText noTextState;
-        private final Text textState;
-        private final TextAfterColon textAfterColonState;
+        private final RegularText textState;
+        private final AfterColon afterColonState;
         private final EntityName entityNameState;
-        private final Parentheses parenthesesState;
+        private final Parenthesis parenthesisState;
         private final StopWord stopWordState;
 
         private FormatterState currentState;
         private String token;
 
         public FormatterStateMachine() {
-            this.noTextState = new NoText(this);
-            this.textState = new Text(this);
-            this.textAfterColonState = new TextAfterColon(this);
+            this.textState = new RegularText(this);
+            this.afterColonState = new AfterColon(this);
             this.entityNameState = new EntityName(this);
-            this.parenthesesState = new Parentheses(this);
+            this.parenthesisState = new Parenthesis(this);
             this.stopWordState = new StopWord(this);
-            this.currentState = this.noTextState;
+            this.currentState = new NoText(this);
         }
 
-        void hasText() {
-            this.currentState.hasText();
+        void regularText() {
+            this.currentState.regularText();
         }
 
-        void hasColon() {
-            this.currentState.hasColon();
+        void colon() {
+            this.currentState.colon();
         }
 
         void entityName() {
             this.currentState.entityName();
         }
 
-        void parentheses() {
-            this.currentState.parentheses();
+        void parenthesis() {
+            this.currentState.parenthesis();
         }
 
         void stopWord() {
@@ -119,21 +149,26 @@ public final class TextFormatter {
             this.token = token;
         }
 
-        String getToken() {
+        String getMappedToken() {
             return this.currentState.mapToken(this.token);
         }
     }
 
     private interface FormatterState {
-        void hasText();
-        void hasColon();
+        void regularText();
+
+        void colon();
+
         void entityName();
-        void parentheses();
+
+        void parenthesis();
+
         void stopWord();
+
         String mapToken(String token);
     }
 
-    private static abstract class AbstractState implements FormatterState {
+    private abstract static class AbstractState implements FormatterState {
 
         private final FormatterStateMachine stateMachine;
 
@@ -142,13 +177,13 @@ public final class TextFormatter {
         }
 
         @Override
-        public void hasText() {
+        public void regularText() {
             this.stateMachine.currentState = this.stateMachine.textState;
         }
 
         @Override
-        public void hasColon() {
-            this.stateMachine.currentState = this.stateMachine.textAfterColonState;
+        public void colon() {
+            this.stateMachine.currentState = this.stateMachine.afterColonState;
         }
 
         @Override
@@ -157,8 +192,8 @@ public final class TextFormatter {
         }
 
         @Override
-        public void parentheses() {
-            this.stateMachine.currentState = this.stateMachine.parenthesesState;
+        public void parenthesis() {
+            this.stateMachine.currentState = this.stateMachine.parenthesisState;
         }
 
         @Override
@@ -181,9 +216,9 @@ public final class TextFormatter {
 
     }
 
-    private static class Text extends AbstractState {
+    private static class RegularText extends AbstractState {
 
-        public Text(FormatterStateMachine stateMachine) {
+        public RegularText(FormatterStateMachine stateMachine) {
             super(stateMachine);
         }
 
@@ -193,14 +228,15 @@ public final class TextFormatter {
         }
     }
 
-    private static class TextAfterColon extends AbstractState {
+    private static class AfterColon extends AbstractState {
 
         private final FormatterStateMachine stateMachine;
 
-        public TextAfterColon(FormatterStateMachine stateMachine) {
+        public AfterColon(FormatterStateMachine stateMachine) {
             super(stateMachine);
             this.stateMachine = stateMachine;
         }
+
         @Override
         public void stopWord() {
             this.stateMachine.currentState = this.stateMachine.textState;
@@ -224,9 +260,9 @@ public final class TextFormatter {
         }
     }
 
-    static class Parentheses extends AbstractState {
+    private static class Parenthesis extends AbstractState {
 
-        public Parentheses(FormatterStateMachine stateMachine) {
+        public Parenthesis(FormatterStateMachine stateMachine) {
             super(stateMachine);
         }
 
@@ -238,12 +274,12 @@ public final class TextFormatter {
                 token = tokenMatcher.group(2);
                 return punctuation + WordUtils.capitalizeFully(token);
             } else {
-                throw new IllegalStateException("Tried to map a non-matching token");
+                throw new IllegalArgumentException("Tried to map a non-matching token");
             }
         }
     }
 
-    static class StopWord extends AbstractState {
+    private static class StopWord extends AbstractState {
 
         public StopWord(FormatterStateMachine stateMachine) {
             super(stateMachine);
