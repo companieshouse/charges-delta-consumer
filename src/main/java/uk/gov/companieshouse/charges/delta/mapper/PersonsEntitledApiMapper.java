38e4aeac5b447d8d4d0babbb2a8af44d6ec9f76a
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-06-13 10:58:02 +0100

    Remove package qualifier

--- src/main/java/uk/gov/companieshouse/charges/delta/mapper/PersonsEntitledApiMapper.java
+++ src/main/java/uk/gov/companieshouse/charges/delta/mapper/PersonsEntitledApiMapper.java
@@ -8,7 +8,7 @@ import uk.gov.companieshouse.api.delta.Person;
 @Mapper(componentModel = "spring")
 public interface PersonsEntitledApiMapper {
 
-    @Mapping(target = "name", expression = "java(uk.gov.companieshouse.charges.delta.mapper."
-            + "TextFormatter.formatAsEntityName(person.getPerson()))")
+    @Mapping(target = "name",
+            expression = "java(TextFormatter.formatAsEntityName(person.getPerson()))")
     PersonsEntitledApi personToPersonsEntitledApi(Person person);
 }
