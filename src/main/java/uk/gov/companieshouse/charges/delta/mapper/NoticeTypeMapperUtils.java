b73e37bb13fb7c040bd1e997cba45b48295bc779
crowlandsCH <crowlands@companieshouse.gov.uk>
2024-10-01 09:47:24 +0100

    DSND-2871: Unused imports

--- src/main/java/uk/gov/companieshouse/charges/delta/mapper/NoticeTypeMapperUtils.java
+++ src/main/java/uk/gov/companieshouse/charges/delta/mapper/NoticeTypeMapperUtils.java
@@ -7,7 +7,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.StringTokenizer;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
